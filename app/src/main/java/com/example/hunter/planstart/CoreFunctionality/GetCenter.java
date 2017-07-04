42368712aa7afcd5a4d8ee6f09bb22f515a80dc0
rahul-soshte <rahul.soshte47@gmail.com>
2017-07-04 18:17:58 +0530

    Action Bar Added,Suggestions Given

--- app/src/main/java/com/example/hunter/planstart/CoreFunctionality/GetCenter.java
+++ app/src/main/java/com/example/hunter/planstart/CoreFunctionality/GetCenter.java
@@ -3,12 +3,13 @@ package com.example.hunter.planstart.CoreFunctionality;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.example.hunter.planstart.Events.EventsOne;
 import com.example.hunter.planstart.HttpHandler;
 import com.example.hunter.planstart.Login.LoginActivity;
+import com.example.hunter.planstart.Places.PlacesOne;
 import com.example.hunter.planstart.R;
 import com.example.hunter.planstart.User.UserOne;
 import com.google.android.gms.maps.CameraUpdate;
@@ -34,13 +35,18 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
-public class GetCenter extends FragmentActivity implements OnMapReadyCallback {
+//Google Places API Web Service
+// AIzaSyDiJ02luwrL_VxUo3E4al2eJqo45mSEzns
+
+public class GetCenter extends AppCompatActivity implements OnMapReadyCallback {
 
     ArrayList<UserOne> user_coordinates=new ArrayList<UserOne>();
 ArrayList<Marker> markers=new ArrayList<>();
+    ArrayList<PlacesOne> places=new ArrayList<>();
+
     private GoogleMap mMap;
      EventsOne event;
-
+String API_KEY="AIzaSyDiJ02luwrL_VxUo3E4al2eJqo45mSEzns";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,80 +78,158 @@ ArrayList<Marker> markers=new ArrayList<>();
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        new GetCenterofUsers().execute();
+     String type="Plotting";
+
+        new GetCenterofUsers().execute(type);
 
 
     }
     private class GetCenterofUsers extends AsyncTask<String,Void,String>
     {
+        String radius="1000";
+        String locationlat;
+        String locationlong;
+
         String result="";
 HttpHandler sh=new HttpHandler();
       String coordinates_url="http://192.168.42.151/Planmap/getcoordinates.php";
-
+String suggestion_url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
         String event_id=Integer.toString(event.getEvent_id());
 
         @Override
-        protected String doInBackground(String... strings) {
-            if (!(LoginActivity.isReachable("192.168.42.151",80,500)))
-            {
-                return "Not Connected or Server Down or No Signal";
+        protected String doInBackground(String... params) {
+            String type=params[0];
+if(type.equals("Plotting")) {
+    if (!(LoginActivity.isReachable("192.168.42.151", 80, 500))) {
+        return "Not Connected or Server Down or No Signal";
+    }
+
+    try {
+        URL url = new URL(coordinates_url);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setDoInput(true);
+        OutputStream outputStream = httpURLConnection.getOutputStream();
+        String post_data = URLEncoder.encode("event_id", "UTF-8") + "=" + URLEncoder.encode(event_id, "UTF-8");
+        sh.WritetoOutputStream(outputStream, post_data);
+        outputStream.close();
+        InputStream inputStream = httpURLConnection.getInputStream();
+        result = sh.convertStreamtoString(inputStream);
+        inputStream.close();
+        httpURLConnection.disconnect();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    if (result != null) {
+        try {
+            JSONArray jsonArray = new JSONArray(result);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                int user_id = 0;
+                double userlat = 0;
+                double userLong = 0;
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                user_id = jsonObject.getInt("user_id_fk");
+                userlat = jsonObject.getDouble("UserLat");
+                userLong = jsonObject.getDouble("UserLong");
+
+                UserOne user = new UserOne(user_id);
+                user.setGpsLatLong(userlat, userLong);
+                user_coordinates.add(user);
             }
-
-            try {
-                URL url=new URL(coordinates_url);
-                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                OutputStream outputStream=httpURLConnection.getOutputStream();
-                String post_data = URLEncoder.encode("event_id","UTF-8")+"="+URLEncoder.encode(event_id,"UTF-8");
-                sh.WritetoOutputStream(outputStream,post_data);
-                outputStream.close();
-                InputStream inputStream=httpURLConnection.getInputStream();
-                result=sh.convertStreamtoString(inputStream);
-                inputStream.close();
-                httpURLConnection.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if(result!=null) {
-try{
-                JSONArray jsonArray = new JSONArray(result);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    int user_id = 0;
-                    double userlat = 0;
-                    double userLong = 0;
-
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    user_id = jsonObject.getInt("user_id_fk");
-                    userlat = jsonObject.getDouble("UserLat");
-                    userLong = jsonObject.getDouble("UserLong");
-
-                    UserOne user = new UserOne(user_id);
-                    user.setGpsLatLong(userlat, userLong);
-                    user_coordinates.add(user);
+        } catch (final JSONException e) {
+            //                Log.e(TAG,"Json Parsing error: "+e);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "Json parsing error:" + e.getMessage(), Toast.LENGTH_LONG).show();
                 }
-            }catch(final JSONException e)
+            });
+
+        }
+    } else {
+        //          Log.e(TAG,"Couldnt get Json from Server");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Couldnt get json from server.Check Logcat for possible errors", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    return "cool";
+}
+if(type.equals("Suggestion"))
+{
+    locationlat=params[1];
+    locationlong=params[2];
+    String jsonStr=sh.makeServiceCall(suggestion_url+locationlat+","+locationlong+"&"+"radius="+radius+"&"+"key="+API_KEY);
+
+    if(jsonStr!=null)
+    {
+        try{
+            JSONObject jsonObj=new JSONObject(jsonStr);
+            //Getting JSON Array node
+            JSONArray results=jsonObj.getJSONArray("results");
+
+            for(int i=0;i<results.length();i++)
             {
-                //                Log.e(TAG,"Json Parsing error: "+e);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(), "Json parsing error:" + e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
+                JSONObject c=results.getJSONObject(i);
+                //  String id=c.getString("id");
+                String name=c.getString("name");
+                String place_id=c.getString("place_id");
 
-            }
-            }else{
-                //          Log.e(TAG,"Couldnt get Json from Server");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),"Couldnt get json from server.Check Logcat for possible errors",Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
+                // String address=c.getString("address");
+                // String gender=c.getString("gender");
+                //pHone node is JSON object
+                //JSONObject phone=c.getJSONObject("phone");
+                JSONObject geometry=c.getJSONObject("geometry");
+                JSONObject location=geometry.getJSONObject("location");
+                double lat=location.getDouble("lat");
+                double longi=location.getDouble("lng");
 
-            return "cool";
+                //String mobile=phone.getString("mobile");
+                //String home=phone.getString("home");
+                //String office=phone.getString("office");
+                //HashMap<String,String> place=new HashMap<>();
+                PlacesOne place=new PlacesOne(name,lat,longi);
+                //contact.put("id",id);
+                    /*
+                    place.put("name",name);
+                    place.put("place_id",place_id);
+                    place.put("long",longi);
+                    place.put("lat",lat);
+                    */
+                //contact.put("mobile",mobile);
+                places.add(place);
+            }
+        }catch(final JSONException e)
+        {
+           // Log.e(TAG,"Json Parsing error: "+e);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),"Json parsing error:"+e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }else{
+        //Log.e(TAG,"Couldnt get Json from Server");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(),"Couldnt get json from server.Check Logcat for possible errors",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    return "Suggested";
+
+}
+
+return "cool";
+
         }
 
 
@@ -154,26 +238,19 @@ protected void onPostExecute(String result)
 {
     if(result.equals("cool"))
     {
-        PlotPoints();
+        PlotUserPoints();
     }
+    if(result.equals("Suggested"))
+    {
+     PlotSuggestionPoints();
+    }
+
 }
     }
 
-/*
-    public LatLng getCenterofPeep()
-    {
-        LatLng center;
-        if(!user_coordinates.isEmpty()){
-            LatLng latLng=new LatLng();
-            LatLngBounds bound = new LatLngBounds.Builder()
-                    .include(user_coordinates).build();
-        }
-        return center;
 
-    }
-    */
 
-    public void PlotPoints()
+    public void PlotUserPoints()
     {
         if(!user_coordinates.isEmpty() && user_coordinates!=null )
         {
@@ -190,27 +267,55 @@ protected void onPostExecute(String result)
             }
 
         }
+
         if(markers.size()==1)
         {
+
             CameraPosition cameraPosition=new CameraPosition.Builder().target(markers.get(0).getPosition()).zoom(12).build();
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            String type="Suggestion";
+            new GetCenterofUsers().execute(type,Double.toString(markers.get(0).getPosition().latitude),Double.toString(markers.get(0).getPosition().longitude));
 
-        }else {
+        }
+        else {
             LatLngBounds.Builder builder = new LatLngBounds.Builder();
 
             for (Marker marker : markers) {
+
                 builder.include(marker.getPosition());
+
             }
 
             LatLngBounds bounds = builder.build();
+
             int padding = 50; // offset from edges of the map in pixels
+
             CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+
             mMap.animateCamera(cu);
+
             LatLng latLng;
+
             latLng = bounds.getCenter();
+
             mMap.addMarker(new MarkerOptions().position(latLng).title("Center"));
+
+            String type="Suggestion";
+
+            new GetCenterofUsers().execute(type,Double.toString(latLng.latitude),Double.toString(latLng.longitude));
 
         }
     }
 
+    public void PlotSuggestionPoints()
+    {
+        int i;
+
+        for(i=0;i<places.size();i++) {
+
+            LatLng latLng = new LatLng(places.get(i).getLatitude(), places.get(i).getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title(places.get(i).getPlaceName()));
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/hunter/planstart/Places/PlacesOne.java
@@ -0,0 +1,38 @@
+package com.example.hunter.planstart.Places;
+
+/**
+ * Created by hunter on 21/2/17.
+ */
+
+public class PlacesOne {
+     String name;
+     double longitude;
+     double latitude;
+public PlacesOne(String name, double latitude, double longitude)
+{
+    this.name=name;
+    this.longitude=longitude;
+    this.latitude=latitude;
+
+}
+public String getPlaceName() {
+    return name;
+
+}
+public void setLongitude(double longitude)
+{this.longitude=longitude;
+}
+    public void setLatitude(double latitude)
+    {
+        this.latitude=latitude;
+    }
+public double getLatitude()
+{
+    return latitude;
+}
+    public double getLongitude()
+    {
+        return longitude;
+
+    }
+}
