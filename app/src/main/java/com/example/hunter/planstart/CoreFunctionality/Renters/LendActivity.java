a91ae5b7bdf37632a0743fe4d70e3d23020878d4
rahul-soshte <rahul.soshte47@gmail.com>
2017-08-03 18:59:47 +0530

    Added ImageUpload Layout to LendActivity

--- app/src/main/java/com/example/hunter/planstart/CoreFunctionality/Renters/LendActivity.java
+++ app/src/main/java/com/example/hunter/planstart/CoreFunctionality/Renters/LendActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.hunter.planstart.R;
-
+//https://www.androidhive.info/2014/12/android-uploading-camera-image-video-to-server-with-progress-bar/
+//https://www.simplifiedcoding.net/android-upload-image-to-server/#comment-12074
 public class LendActivity extends AppCompatActivity {
 
     @Override
